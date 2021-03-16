654afd9791d68ceeeadc0de21e1841e6c9a70307
이지훈 <easyhoon119@github.com>
2021-03-16 22:45:56 +0900

    fix error

--- main.js
+++ main.js
@@ -112,9 +112,9 @@ function card_setting(karo, sero) {
     container2.appendChild(hide);
     document.querySelector('.background').appendChild(container2);
     document.querySelectorAll('.card').forEach(function (card, index) {
-        document.addEventListener('DOMContentLoaded', setTimeout(function () {
+        setTimeout(function () {
             card.classList.add('flipped');
-        }, 2000 + 100 * index))
+        }, 2000 + 100 * index);
     });
 
     setTimeout(function () {
